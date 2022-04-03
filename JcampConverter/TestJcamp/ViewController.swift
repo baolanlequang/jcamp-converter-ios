b73f81ce452ce5ebf0d18f8b53b3e867fdcdde31
baolan2005 <baolan2005@gmail.com>
2022-04-03 21:37:15 +0200

    ad test files

--- JcampConverter/TestJcamp/ViewController.swift
+++ JcampConverter/TestJcamp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let path = Bundle.main.path(forResource: "test_file_1", ofType: "dx") {
+        if let path = Bundle.main.path(forResource: "File012", ofType: "dx") {
             let reader = JcampReader(filePath: path)
             for spec in reader.jcamp!.spectra {
                 print(spec.xValues.count)
